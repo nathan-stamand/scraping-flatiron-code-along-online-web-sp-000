1d1038eebc11c5b68625694901ba9836a5cdfc0d
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-01 01:51:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,9 +15,8 @@ class Scraper
       course.schedule = post.css("date").text 
       course.description = post.css("p").text 
     end 
-    binding.pry
   end
-  
+  binding.pry
   def print_courses
     self.make_courses
     Course.all.each do |course|
